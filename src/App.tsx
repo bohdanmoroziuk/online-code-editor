1d829a5a0dca5da605911b00b5495f4bb51bb0d7
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-06-29 16:31:56 +0300

    feat: add panes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import { FC } from 'react';
 
+import Editor from 'components/Editor';
+
 const App: FC = () => (
   <div className="app">
-    App
+    <div className="pane top-pane">
+      <Editor />
+      <Editor />
+      <Editor />
+    </div>
+    <div className="pane">
+      <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+    </div>
   </div>
 );
 
---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import 'components/Editor/index.css';
+
+const Editor: FC = () => (
+  <div className="editor"></div>
+);
+
+export default Editor;
