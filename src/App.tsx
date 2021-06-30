0273bce4ca756478565ecccbf7475e95f2970f48
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-06-30 09:11:43 +0300

    feat: setup editors

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import Editor from 'components/Editor';
 
-const App: FC = () => (
-  <div className="app">
-    <div className="pane top-pane">
-      <Editor />
-      <Editor />
-      <Editor />
+const App: FC = () => {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
+  return (
+    <div className="app">
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </div>
-    <div className="pane">
-      <iframe
-        title="output"
-        sandbox="allow-scripts"
-        frameBorder="0"
-        width="100%"
-        height="100%"
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,9 +1,48 @@
 import { FC } from 'react';
 
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+
 import 'components/Editor/index.css';
 
-const Editor: FC = () => (
-  <div className="editor"></div>
-);
+interface Props {
+  displayName: 'HTML' | 'CSS' | 'JS';
+  language: 'xml' | 'css' | 'javascript';
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const Editor: FC<Props> = ({ displayName, language, value, onChange }) => {
+  const handleChange = (_editor: any, _data: any, value: string) => {
+    onChange(value);
+  };
+
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O\C</button>
+      </div>
+      <ControlledEditor 
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true,
+        }}
+      />
+    </div>
+  );
+};
 
 export default Editor;
